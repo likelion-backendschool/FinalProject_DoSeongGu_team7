a2cec5acefe2d8f2aff3ad01cc0a6d2850f1466f
SG1515 <sunggu507@naver.com>
2022-10-19 17:50:20 +0900

    제출하기 위해 삭제

--- 1Week_Mission/src/main/java/com/ll/exam/FinalProject/app/member/exception/EmailDuplicatedException.java
+++
@@ -1,7 +0,0 @@
-package com.ll.exam.FinalProject.app.member.exception;
-
-public class EmailDuplicatedException extends RuntimeException {
-    public EmailDuplicatedException(String message) {
-        super(message);
-    }
-}
--- 1Week_Mission/src/main/java/com/ll/exam/FinalProject/app/member/exception/UsernameDuplicatedException.java
+++
@@ -1,7 +0,0 @@
-package com.ll.exam.FinalProject.app.member.exception;
-
-public class UsernameDuplicatedException extends RuntimeException {
-    public UsernameDuplicatedException(String message) {
-        super(message);
-    }
-}
